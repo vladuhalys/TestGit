c8df7b36c4c7f73d1023141196313d66dfee555b
Lysenko Vladyslav <vlad.lysenko1999@gmail.com>
2023-11-18 14:25:11 +0200

    1) Create array on 4 elements for function 2) Create the function sum 3) Finish this task

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
+template<typename T>
+T sum(T* values, int amount)
+{
+	T sum = 0;
+	for (size_t i = 0; i < amount; i++)
+	{
+		sum += values[i];
+	}
+	return sum;
+}
+
 int main()
 {
+	int size = 4;
+	int* _array = new int[size] {1,2,3,4};
+	int result = sum<int>(_array, size);
 
+	cout << "Sum = " << result << endl;
 	return 0;
 }
